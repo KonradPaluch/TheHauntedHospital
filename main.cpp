d353615c8e8325ca330c7b31d8a3f8f8481bb531
KonradPaluch <konradpaluch97@gmail.com>
2018-11-14 21:30:33 -0600

    Added: - timer of bleeding out - third dimension of the rooms

--- main.cpp
+++ main.cpp
@@ -37,22 +37,24 @@ struct room{
  * loop for each room
  * template of the file in the file
  */
-void defineHospital(room oRooms[3][3]){
+void defineHospital(room oRooms[3][3][2]){
     ifstream fMyFile("/Users/konradpaluch/CLionProjects/TheHauntedHospital/RoomContent.txt");
 
-    for (int iRow = 0; iRow < 3; iRow++){
-        for (int iCol = 0; iCol < 3; iCol++){
-            getline(fMyFile, oRooms[iRow][iCol].sDescription1);
-            getline(fMyFile, oRooms[iRow][iCol].sDescription2);
-            getline(fMyFile, oRooms[iRow][iCol].sDescription3);
-            getline(fMyFile, oRooms[iRow][iCol].sOldMoves);
-            getline(fMyFile, oRooms[iRow][iCol].sNewMoves);
-            fMyFile >> oRooms[iRow][iCol].bActionDone;
-            fMyFile >> oRooms[iRow][iCol].bActionPossible;
-            fMyFile >> oRooms[iRow][iCol].bAffectsItself;
-            fMyFile >> oRooms[iRow][iCol].cActionToDo;
-            fMyFile >> oRooms[iRow][iCol].iAffectedRoomRow;
-            fMyFile >> oRooms[iRow][iCol].iAffectedRoomCol;
+    for (int iLevel = 0; iLevel < 2; iLevel++) {
+        for (int iRow = 0; iRow < 3; iRow++) {
+            for (int iCol = 0; iCol < 3; iCol++) {
+                getline(fMyFile, oRooms[iRow][iCol][iLevel].sDescription1);
+                getline(fMyFile, oRooms[iRow][iCol][iLevel].sDescription2);
+                getline(fMyFile, oRooms[iRow][iCol][iLevel].sDescription3);
+                getline(fMyFile, oRooms[iRow][iCol][iLevel].sOldMoves);
+                getline(fMyFile, oRooms[iRow][iCol][iLevel].sNewMoves);
+                fMyFile >> oRooms[iRow][iCol][iLevel].bActionDone;
+                fMyFile >> oRooms[iRow][iCol][iLevel].bActionPossible;
+                fMyFile >> oRooms[iRow][iCol][iLevel].bAffectsItself;
+                fMyFile >> oRooms[iRow][iCol][iLevel].cActionToDo;
+                fMyFile >> oRooms[iRow][iCol][iLevel].iAffectedRoomRow;
+                fMyFile >> oRooms[iRow][iCol][iLevel].iAffectedRoomCol;
+            }
         }
     }
     fMyFile.close();
@@ -91,15 +93,15 @@ int menuDisplay(){
  * process: checks if bActionDone, if bActionPossible, shows according description
  * output: none
  */
-void showDescription(room oRooms[3][3], int iPlayerRow, int iPlayerCol){
-    if (oRooms[iPlayerRow][iPlayerCol].bActionDone){
-        cout << oRooms[iPlayerRow][iPlayerCol].sDescription3 << endl;
+void showDescription(room oRooms[3][3][2], int iPlayerRow, int iPlayerCol, int iPlayerLevel){
+    if (oRooms[iPlayerRow][iPlayerCol][iPlayerLevel].bActionDone){
+        cout << oRooms[iPlayerRow][iPlayerCol][iPlayerLevel].sDescription3 << endl;
     }
-    else if (oRooms[iPlayerRow][iPlayerCol].bActionPossible){
-        cout << oRooms[iPlayerRow][iPlayerCol].sDescription2;
+    else if (oRooms[iPlayerRow][iPlayerCol][iPlayerLevel].bActionPossible){
+        cout << oRooms[iPlayerRow][iPlayerCol][iPlayerLevel].sDescription2 << endl;
     }
     else {
-        cout << oRooms[iPlayerRow][iPlayerCol].sDescription1;
+        cout << oRooms[iPlayerRow][iPlayerCol][iPlayerLevel].sDescription1 << endl;
     }
 }
 
@@ -108,29 +110,39 @@ void showDescription(room oRooms[3][3], int iPlayerRow, int iPlayerCol){
  * takes the player's input, checks if it is a possible action, if yes it modifies one of the bool in certain room,
  * if not an action, checks if possible movement, if not, asks for input again;
  */
-void takeMake(room oRooms[3][3], int &iPlayerRow, int &iPlayerCol){
+void takeMake(room oRooms[3][3][2], int &iPlayerRow, int &iPlayerCol, int &iPlayerLevel){
     char cDecision;
     cin >> cDecision;
-
-    if ((cDecision == oRooms[iPlayerRow][iPlayerCol].cActionToDo) && (oRooms[iPlayerRow][iPlayerCol].bActionPossible)) {
-        if (oRooms[iPlayerRow][iPlayerCol].bAffectsItself) {
-            oRooms[iPlayerRow][iPlayerCol].bActionDone = true;
-            oRooms[iPlayerRow][iPlayerCol].bActionPossible = false;
+    /*
+    locale loc;
+    toupper(cDecision, loc);
+    */
+    //going up or down from the elevator
+    if (iPlayerRow == 2 && iPlayerCol == 2 && iPlayerLevel == 0 && cDecision == 'U'){
+        iPlayerLevel += 1;
+    }
+    else if (iPlayerRow == 2 && iPlayerCol == 2 && iPlayerLevel == 1 && cDecision == 'D'){
+        iPlayerLevel -= 1;
+    }
+    else if ((cDecision == oRooms[iPlayerRow][iPlayerCol][iPlayerLevel].cActionToDo) && (oRooms[iPlayerRow][iPlayerCol][iPlayerLevel].bActionPossible)) {
+        if (oRooms[iPlayerRow][iPlayerCol][iPlayerLevel].bAffectsItself) {
+            oRooms[iPlayerRow][iPlayerCol][iPlayerLevel].bActionDone = true;
+            oRooms[iPlayerRow][iPlayerCol][iPlayerLevel].bActionPossible = false;
         }
         else {
-            oRooms[oRooms[iPlayerRow][iPlayerCol].iAffectedRoomRow][oRooms[iPlayerRow][iPlayerCol].iAffectedRoomCol].bActionPossible = true;
-            oRooms[iPlayerRow][iPlayerCol].bActionDone = true;
-            oRooms[iPlayerRow][iPlayerCol].bActionPossible = false;
+            oRooms[oRooms[iPlayerRow][iPlayerCol][iPlayerLevel].iAffectedRoomRow][oRooms[iPlayerRow][iPlayerCol][iPlayerLevel].iAffectedRoomCol][iPlayerLevel].bActionPossible = true;
+            oRooms[iPlayerRow][iPlayerCol][iPlayerLevel].bActionDone = true;
+            oRooms[iPlayerRow][iPlayerCol][iPlayerLevel].bActionPossible = false;
         }
     }
-    else if (oRooms[iPlayerRow][iPlayerCol].bActionDone){
-        if (oRooms[iPlayerRow][iPlayerCol].sNewMoves.find(cDecision) != oRooms[iPlayerRow][iPlayerCol].sNewMoves.npos){
+    else if (oRooms[iPlayerRow][iPlayerCol][iPlayerLevel].bActionDone){
+        if (oRooms[iPlayerRow][iPlayerCol][iPlayerLevel].sNewMoves.find(cDecision) != oRooms[iPlayerRow][iPlayerCol][iPlayerLevel].sNewMoves.npos){
             switch (cDecision) {
                 case 'N':
-                    iPlayerRow += 1;
+                    iPlayerRow -= 1;
                     break;
                 case 'S':
-                    iPlayerRow -= 1;
+                    iPlayerRow += 1;
                     break;
                 case 'W':
                     iPlayerCol -= 1;
@@ -143,7 +155,7 @@ void takeMake(room oRooms[3][3], int &iPlayerRow, int &iPlayerCol){
             }
         }
     }
-    else if (oRooms[iPlayerRow][iPlayerCol].sOldMoves.find(cDecision) != oRooms[iPlayerRow][iPlayerCol].sOldMoves.npos){
+    else if (oRooms[iPlayerRow][iPlayerCol][iPlayerLevel].sOldMoves.find(cDecision) != oRooms[iPlayerRow][iPlayerCol][iPlayerLevel].sOldMoves.npos){
         switch (cDecision) {
             case 'N':
                 iPlayerRow -= 1;
@@ -164,7 +176,7 @@ void takeMake(room oRooms[3][3], int &iPlayerRow, int &iPlayerCol){
     else {
         cin.clear();
         cin.ignore();
-        cout << "This action is not possible. Please choose another one.";
+        cout << "This action is not possible. Please choose another one." << endl;
     }
 }
 
@@ -173,20 +185,30 @@ void takeMake(room oRooms[3][3], int &iPlayerRow, int &iPlayerCol){
  * input: whole matrix
  * shows explanation, possible decisions, takes decision and alters players position
  */
-void game(room oRooms[3][3]){
-    cout << "In this game, you make decisions by typing in the first letter of the action you want to do, \nor the letter of the direction you want to go to. \n";
-    int iPlayerRow = 2;
+void game(room oRooms[3][3][2]) {
+    cout << "In this game, you make decisions by typing in the first letter of the action you want to do, \nor the letter of the direction you want to go to.\n\n";
+    int iPlayerRow = 0;
     int iPlayerCol = 1;
+    int iPlayerLevel = 0;
+    int iBleedingOut = 0;
+    while (!oRooms[0][0][0].bActionDone) {
+        //timer 
+        if (iBleedingOut == 15) {
+            oRooms[0][0][0].bActionDone = true;
+        }
+        showDescription(oRooms, iPlayerRow, iPlayerCol, iPlayerLevel);
+        takeMake(oRooms, iPlayerRow, iPlayerCol, iPlayerLevel);
+        //oRooms[0][0][0].bActionDone = true;
+        iBleedingOut++;
+    }
+    if (iBleedingOut < 15) {
+        cout << "Congrats you have finished the game." << endl;
+    }
+    else {
+        cout << "You are bleeding to death. The walls are starting to get blurry as you try to hold on to them. \nYou fall to the ground and die\n";
+    }
 
-      while (!oRooms[0][0].bActionDone) {
-          showDescription(oRooms, iPlayerRow, iPlayerCol);
-          takeMake(oRooms, iPlayerRow, iPlayerCol);
-          oRooms[0][0].bActionDone = true;
-      }
-
-    cout << "Congrats you have finished the game." << endl;
 }
-
 
 int main(){
 
@@ -197,28 +219,27 @@ int main(){
     bool bRun = true;
 
     //matrix of story
-    room oRooms[3][3];
+    room oRooms[3][3][2];
 
     //defining our story
     defineHospital(oRooms);
 
     //menu loop
-    while (bRun){
+    while (bRun) {
 
         iMenuDecision = menuDisplay();
 
-        switch (iMenuDecision){
+        switch (iMenuDecision) {
             //menu decision 1 means we want to play the game
             case 1:
                 game(oRooms);
                 break;
 
-            //menu decision 2 means we want to exit program
+                //menu decision 2 means we want to exit program
             case 2:
                 bRun = false;
-                cout << "Thank you for playing the Haunted Hospital, \na game made by Marie-lou Memelink and Konrad Paluch";
+                cout << "Thank you for playing the Haunted Hospital, \na game made by Marie-lou Memelink and Konrad Paluch" << endl;
                 break;
-
             default:
                 break;
         }
